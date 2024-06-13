2e107896524f071997a67f140bcb527322ed5f1e
Momitul Hoque Chowdhury <110973021+Momit87@users.noreply.github.com>
2024-06-13 10:01:11 +0600

    Add files via upload

---
+++ FIFO.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+
+   
+    cin >>n;
+    vector<pair<pair<int, int>, char>> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first.first >> v[i].first.second;
+        v[i].second = 'A' + i;
+    }
+    sort(v.begin(), v.end());
+     cout << "----------------Gantt Chart-----------------" << endl;
+    cout << "Time  : Character " << endl;
+    int time=0;
+    for(int i=0;i<n;i++){
+        time=max(v[i].first.first,time);
+        time+=v[i].first.second;
+        cout<<time<<' '<<v[i].second<<endl;
+    }
+    
+    cout<<"Times : "<<time<<endl;
+    
+
+    return 0;
+}
